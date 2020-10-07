91094420b1133cb635b6b878f3c7f2fcfb793dd5
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-10-07 13:59:43 -0700

    game is working

--- src/Board.js
+++ src/Board.js
@@ -33,58 +33,65 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
   function createBoard() {
     let initialBoard = [];
-    const values = [".", ".", 0, ".", ".", 0, ".", ".", "."];
-    const genRandomNum = () => {
-      return Math.floor((Math.random() * values.length))
-    }
-    for (let i = 0; i < ncols; i++) {
-      const innerArray = []
-      for (let i = 0; i < nrows; i++) {
-        innerArray.push(values[genRandomNum()])
+    for (let i = 0; i < nrows; i++) {
+      const row = []
+      for (let j = 0; j < ncols; j++) {
+        row.push(Math.random() < chanceLightStartsOn)
       }
-      initialBoard[i] = innerArray;
+      initialBoard.push(row);
     }
+    console.log(initialBoard);
     return initialBoard;
   }
 
   function hasWon() {
-    // TODO: check the board in state to determine whether the player has won.
-    let hasWon = true;
-    for (let row of board) {
-      if (row.includes(0)) {
-        hasWon = false;
-      }
-    }
-    return hasWon;
+    return board.every(row => row.every(cell => !cell));
   }
 
   function flipCellsAround(coord) {
     setBoard(oldBoard => {
       const [y, x] = coord.split("-").map(Number);
-
       const flipCell = (y, x, boardCopy) => {
-        // if this coord is actually on board, flip it
-
         if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
           boardCopy[y][x] = !boardCopy[y][x];
         }
       };
-
-      // TODO: Make a (deep) copy of the oldBoard
-
-      // TODO: in the copy, flip this cell and the cells around it
-
-      // TODO: return the copy
+      const boardCopy = oldBoard.map(row => [...row])
+      flipCell(y, x, boardCopy);
+      flipCell(y, x - 1, boardCopy);
+      flipCell(y, x + 1, boardCopy);
+      flipCell(y - 1, x, boardCopy);
+      flipCell(y + 1, x, boardCopy);
+      return boardCopy;
     });
   }
 
-  // if the game is won, just show a winning msg & render nothing else
+  if (hasWon()) {
+    return <div> <h1>You Won!</h1></div>
+  }
 
-  // TODO
+  let tableBoard = [];
 
-  // make table board
+  for (let y = 0; y < nrows; y++) {
+    let row = [];
+    for (let x = 0; x < ncols; x++) {
+      let coord = `${y}-${x}`;
+      row.push(
+        <Cell
+          key={coord}
+          isLit={board[y][x]}
+          flipCellsAroundMe={() => flipCellsAround(coord)}
+        />
+      );
+    }
+    tableBoard.push(<tr key={y}>{row}</tr>);
+  }
 
-  // TODO
+  return (
+    <table className="Board">
+    <tbody>{tableBoard}</tbody>
+  </table>
+  )
 }
 
 export default Board;
--- src/practice.js
+++ src/practice.js
@@ -1,20 +1,20 @@
-function Board(nrows, ncols) {
+// function Board(nrows, ncols) {
   
-    /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
-    function createBoard() {
-      let initialBoard = [];
-      const values = [0, ".", 0, 0, 0, 0, 0, 0, 0];
-      const genRandomNum = () => {
-        return Math.floor((Math.random() * values.length))
-      }
-      for (let i = 0; i < ncols; i++) {
-        const innerArray = []
-        for (let i = 0; i < nrows; i++) {
-          innerArray.push(values[genRandomNum()])
-        }
-        initialBoard[i] = innerArray;
-      }
-      return initialBoard;
-    }
-}
+//     /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
+//     function createBoard() {
+//       let initialBoard = [];
+//       const values = [0, ".", 0, 0, 0, 0, 0, 0, 0];
+//       const genRandomNum = () => {
+//         return Math.floor((Math.random() * values.length))
+//       }
+//       for (let i = 0; i < ncols; i++) {
+//         const innerArray = []
+//         for (let i = 0; i < nrows; i++) {
+//           innerArray.push(values[genRandomNum()])
+//         }
+//         initialBoard[i] = innerArray;
+//       }
+//       return initialBoard;
+//     }
+// }
 
